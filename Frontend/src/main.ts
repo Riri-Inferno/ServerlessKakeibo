dcfc6833c852a4b3608e0a7bd29e6defbb416f6d
T.U <takayo.uenter36@icloud.com>
2026-01-19 01:49:46 +0900

    グーグルログイン用のSDK入れた

--- Frontend/src/main.ts
+++ Frontend/src/main.ts
@@ -2,7 +2,14 @@ import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
 import router from "./router/index.ts";
+import vue3GoogleLogin from "vue3-google-login";
 
 const app = createApp(App);
+
+// Google Login 初期化
+app.use(vue3GoogleLogin, {
+  clientId: import.meta.env.VITE_GOOGLE_CLIENT_ID,
+});
+
 app.use(router);
 app.mount("#app");
