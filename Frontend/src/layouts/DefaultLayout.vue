b8c8c791385f48da4fc1b1f47bc016a012ad743f
T.U <takayo.uenter36@icloud.com>
2026-01-22 02:42:20 +0900

    レイアウトからクラス削除

--- Frontend/src/layouts/DefaultLayout.vue
+++ Frontend/src/layouts/DefaultLayout.vue
@@ -16,17 +16,13 @@ const toggleSidebar = () => {
     <AppHeader @toggle-sidebar="toggleSidebar" class="flex-shrink-0" />
 
     <div class="flex flex-1 overflow-hidden min-h-0">
-      <AppSidebar
-        :is-open="isSidebarOpen"
-        @close="isSidebarOpen = false"
-        class="flex-shrink-0"
-      />
+      <AppSidebar :is-open="isSidebarOpen" @close="isSidebarOpen = false" />
 
       <main class="flex-1 overflow-y-auto p-4 md:p-6 lg:p-8 pb-24 md:pb-6">
         <slot />
       </main>
     </div>
 
-    <AppBottomNav class="flex-shrink-0" />
+    <AppBottomNav />
   </div>
 </template>
