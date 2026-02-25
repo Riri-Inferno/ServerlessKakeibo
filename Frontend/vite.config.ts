ae0da2f745884fbcabb09b744da31b55c290acfd
T.U <takayo.uenter36@icloud.com>
2026-02-25 14:29:36 +0900

    ベースパスを明示的に設定

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -1,17 +1,18 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.VITE_BASE_PATH || "/",
   plugins: [vue()],
   server: {
     host: true, // Dockerコンテナ内でアクセス可能
     port: 5173,
     proxy: {
-      '/api': {
-        target: process.env.VITE_API_BASE_URL || 'http://localhost:8080',
+      "/api": {
+        target: process.env.VITE_API_BASE_URL || "http://localhost:8080",
         changeOrigin: true,
-      }
-    }
-  }
-})
+      },
+    },
+  },
+});
