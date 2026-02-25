c52b26634a6608bc0c68c3763fe27eaf2d3edf3b
T.U <takayo.uenter36@icloud.com>
2026-02-25 15:56:34 +0900

    ルーターの設定を修正し、認証が必要なルートの管理を改善

--- Frontend/src/router/index.ts
+++ Frontend/src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import { useAuthStore } from "../stores/authStore";
 import ComponentGallery from "../pages/ComponentGallery.vue";
 import Login from "../pages/Login.vue";
@@ -9,7 +9,7 @@ import Settings from "../pages/Settings.vue";
 import AuthCallback from "../pages/AuthCallback.vue";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: "/",
